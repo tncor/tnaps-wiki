83909b99e275c7d2d869ad072005033d95ca633f
vkovalskiy <kovalskiy@tncor.com>
2011-12-04 02:38:49 +1100

    Regenerated diagrams and extended generation script

---
+++ img/diagrams/rb.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+#
+#
+ARGV.each do|a|
+	  puts "Argument: #{a}"
+end
+
+puts "wow " + ARGV[0] + "kb"
--- img/diagrams/yuml.rb
+++ img/diagrams/yuml.rb
@@ -1,6 +1,12 @@
+#!/usr/bin/ruby
+#
 require 'rubygems'
 require 'curb'
 require 'rio' 
+
+name = ARGV[0]
+yuml_name = name + ".yuml"
+img_name = name + ".png"
 
 def get_file_as_string(filename)
 	data = ''
@@ -14,12 +20,12 @@ end
 
 # POST DSL up to yUML
 #c = Curl::Easy.new("http://yuml.me/diagram/scruffy/class/")
-c = Curl::Easy.new("http://yuml.me/diagram/scruffy;dir:TB;scale:50;/class/")
+c = Curl::Easy.new("http://yuml.me/diagram/scruffy;scale:80;/class/")
 c.multipart_form_post = true
-file_data = get_file_as_string 'architecture.yuml'
+file_data = get_file_as_string yuml_name
 c.http_post(Curl::PostField.content("dsl_text",file_data))
 
 # Grab the ID, and then download img and save to disk
 img_id = c.body_str
 img = "http://yuml.me/#{img_id}"
-rio(img) > rio("img.png") 
+rio(img) > rio(img_name) 
